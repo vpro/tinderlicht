2a024a998bccd8e7e8d91958414b650cafbdf476
Zaturrby <rjkorteschiel@gmail.com>
2015-10-16 18:31:07 +0200

    change

--- src/components/iconclass/Iconclass.jsx
+++ src/components/iconclass/Iconclass.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import request from 'superagent';
+import iconClass from '../../data/iconclass/datakeys.json';
 
 class Iconclass extends React.Component{
     render() {
@@ -10,6 +10,7 @@ class Iconclass extends React.Component{
 			<div className="iconclass-container">
 				<h3 className="waaris-kop">Waar is?</h3>
 				{iconclassArray.map((value, index) => {
+					console.log(iconClass[value.$t]);
 					return (
 			       	 	<p key={index} className="iconclass">{value.$t}</p>
 					)
