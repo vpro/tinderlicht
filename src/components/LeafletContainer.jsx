4be76ba4d7d74b36d5f934b777cbfb51172274c5
Zaturrby <rjkorteschiel@gmail.com>
2015-10-17 14:43:48 +0200

    changes

---
+++ src/components/LeafletContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import request from 'superagent';
+
+class LeafletContainer extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			lodData: null
+		}
+	}
+
+	componentDidMount() {
+    	var lodURL = 'http://lod.kb.nl/rce/A30FCAFC-A206-4F2B-84CB-9CB2232E8B85?output=application/ld+json';
+    	var callback = function(err, res){
+    		var parsedRes = JSON.parse(res.text);
+    		this.setState({lodData: parsedRes})
+    	}.bind(this);
+    	request
+    		.get(lodURL)
+	    	.end(callback);
+	}
+	
+    render() {
+    	if (this.state.lodData){
+			var position = [ this.state.lodData.lat, this.state.lodData.long]
+		}
+        return (
+	        <Map className="map-container" center={position} zoom={12}>
+	    		<TileLayer
+	      			url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+	    		/>
+	    		<Marker position={position} />
+	  		</Map>
+  		);
+    }
+}
+
+export default LeafletContainer;
