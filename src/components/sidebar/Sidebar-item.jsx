1c734be7a73667d7158aa564533f447894290ad4
Zaturrby <rjkorteschiel@gmail.com>
2015-10-17 09:32:01 +0200

    first number

--- src/components/sidebar/Sidebar-item.jsx
+++ src/components/sidebar/Sidebar-item.jsx
@@ -6,8 +6,8 @@ class SidebarItem extends React.Component{
 
     	var userData = this.props.userData;
     	var records = this.props.verluchtingen.srw$searchRetrieveResponse.srw$records.srw$record;
-
-    	if (this.props.number < userData.gameData.position){
+    	console.log(this.props.number, userData.gameData.position);
+    	if (this.props.number <= userData.gameData.position){
     		var thumbnail = records[(userData.gameData.position - this.props.number)].srw$recordData.srw_dc$dc.dcx$thumbnail.$t;
 			var divStyle = {
 				background: "url(" + (thumbnail)+ ")"
