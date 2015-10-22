a12617c90a49a7a963121928b28fd5017cb9916c
Zaturrby <rjkorteschiel@gmail.com>
2015-10-22 12:09:38 +0200

    styles are loading

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import "babel/polyfill";
-import "./styles/app.scss";
+import "./assets/styles/app.scss";
 
 import HackingHistorians from "./components/HackingHistorians.jsx";
 
