9e9a5a980a8c778b3a62263c1980f83749235c18
evanzummeren <erikvanzummeren@gmail.com>
2016-02-02 12:48:11 +0100

    gh page deploy

---
+++ ghdeploy.sh
@@ -0,0 +1,8 @@
+npm run deploy
+git checkout gh-pages
+cp -a $HOME/Desktop/Tinderlicht/build/* $HOME/Desktop/Tinderlicht/
+git status
+git add -A
+git commit -m "gh page deploy"
+git push
+git checkout master
