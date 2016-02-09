a746d4b9da077665158fde047453cdb03ed87352
evanzummeren <erikvanzummeren@gmail.com>
2016-02-09 14:22:11 +0100

    div to a tag

--- src/components/Tinderlicht.jsx
+++ src/components/Tinderlicht.jsx
@@ -191,8 +191,8 @@ class Tinderlicht extends React.Component{
     			<div className="app-container">
     				<div className="auth-container">
     					<Auth />
-    						<div className="auth-button auth-sm facebook" onClick={this.login.bind(this, 'facebook')}>Login met Facebook</div>
-    						<div className="auth-button auth-sm twitter" onClick={this.login.bind(this, 'twitter')}>Login met Twitter</div>
+    						<a className="auth-button auth-sm facebook" onClick={this.login.bind(this, 'facebook')}>Login met Facebook</a>
+    						<a className="auth-button auth-sm twitter" onClick={this.login.bind(this, 'twitter')}>Login met Twitter</a>
     				</div>
     			</div>
     		);
